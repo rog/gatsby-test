c7eededd65e4b44d4286357f24fffab1f15e97c0
Rogelio Alberto <i@rog.mx>
2020-10-10 17:09:46 -0500

    Get Data from sanity

--- gatsby/src/pages/pizza.js
+++ gatsby/src/pages/pizza.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-export default function PizzaPage () {
+import PizzaList from '../components/PizzaList'
+
+export default function PizzaPage ({data}) {
     return (<>
-        <p>Hello! I'm pizzaa pageeeee!.</p>
+        <PizzaList pizzas={data.pizzas.nodes}/>
     </>);
 }
+
+export const query = graphql`
+    query PizzaQuery {
+        pizzas: allSanityPizza {
+            nodes {
+                id
+                name
+                price
+                image {
+                    asset {
+                        fixed(width: 200, height: 200) {
+                            ...GatsbySanityImageFixed
+                        }
+                        fluid(maxWidth: 400) {
+                            ...GatsbySanityImageFluid
+                        }
+                    }
+                }
+                slug {
+                    current
+                }
+                toppings {
+                    id
+                    name
+                }
+            }
+        }
+    }
+`
