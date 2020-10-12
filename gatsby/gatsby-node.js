58a8a827a2a603521cb967a017f77b16c4a39f8e
Rogelio Alberto <i@rog.mx>
2020-10-12 04:30:25 -0500

    Beers API

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,5 +1,6 @@
 import { promises } from 'fs'
 import path from 'path'
+import 'isomorphic-fetch'
 
 async function turnPizzasIntoPages ({graphql, actions}) {
     const PizzaTemplate = path.resolve('./src/templates/Pizza.js')
@@ -50,9 +51,66 @@ async function turnToppingsIntoPages ({graphql, actions}) {
     })
 }
 
+async function fetchBeersAndTurnIntoNodes ({actions, createNodeId, createContentDigest}) {
+    const res = await fetch('https://sampleapis.com/beers/api/stouts')
+    const beers = await res.json()
+    for (const beer of beers) {
+        const nodeMeta = {
+            id: createNodeId(`beer-${beer.name}`),
+            parent: null,
+            children: [],
+            internal: {
+                type: 'Beer',
+                mediaType: 'application/json',
+                contentDigest: createContentDigest(beer)
+            }
+        }
+        actions.createNode({
+            ...beer,
+            ...nodeMeta
+        })
+    }
+}
+
+async function turnSlicemastersIntoPages ({graphql, actions}) {
+    const { data } = await graphql(`
+        query {
+            slicemasters: allSanityPerson {
+                totalCount
+                nodes {
+                    name
+                    id
+                    slug {
+                        current
+                    }
+                }
+            }
+        }
+    `)
+    const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE)
+    const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize)
+
+    Array.from({length: pageCount}).forEach((_, i) => {
+        actions.createPage({
+            path: `/slicemasters/${i + 1}`,
+            component: path.resolve('./src/pages/slicemasters.js'),
+            context: {
+                skip: i * pageSize,
+                currentPage: i + 1,
+                pageSize
+            }
+        })
+    })
+}
+
+export async function sourceNodes (params) {
+    await Promise.all([fetchBeersAndTurnIntoNodes(params)])
+}
+
 export async function createPages (params) {
     await Promise.all([
         turnPizzasIntoPages(params),
-        turnToppingsIntoPages(params)
+        turnToppingsIntoPages(params),
+        turnSlicemastersIntoPages(params)
     ])
 }
